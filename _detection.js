1bc8b4bf82beeb73db30c4d16a0e3cfbb67aae77
interfacteur <interfacteur@users.noreply.github.com>
2014-07-28 22:43:53 +0200

    commentaire

--- _detection.js
+++ _detection.js
@@ -1,9 +1,10 @@
-	var targetant = (function() { //Détection du support de la pseudo-classe ':target' - réf. http://www.zachleat.com/test/css-target-feature-test/querySelector.html
-		if (Object.prototype.toString.call(window.operamini) !== "[object OperaMini]" && "querySelectorAll" in document) {
-			try {
-				document.querySelectorAll(":target");
-				return true;
-			} catch(e){}
-		}
-		return false;
-	})();
+//Détection du support de la pseudo-classe ':target' - réf. http://www.zachleat.com/test/css-target-feature-test/querySelector.html
+var targetant = (function() {
+	if (Object.prototype.toString.call(window.operamini) !== "[object OperaMini]" && "querySelectorAll" in document) {
+		try {
+			document.querySelectorAll(":target");
+			return true;
+		} catch(e){}
+	}
+	return false;
+})();
